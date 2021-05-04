aaef131d3f1abfbb5cf8826f3737c6732777deed
ashencat <forddagujar@gmail.com>
2021-05-04 18:08:34 -0400

    working titlebar actions

--- preload.js
+++ preload.js
@@ -1,10 +1,15 @@
 const { ipcRenderer, contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-    notificationApi: {
-        sendNotification(message) {
-            console.log(message)
-            ipcRenderer.send('notify', message)
+    // notificationApi: {
+    //     sendNotification(message) {
+    //         console.log(message)
+    //         ipcRenderer.send('notify', message)
+    //     }
+    // },
+    windowApi: {
+        windowState(action) {
+            ipcRenderer.send('windowState', action)
         }
     }
 })
