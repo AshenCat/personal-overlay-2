1f2bfdd17df91fe6139a037c77a9bd6743d4059d
ashencat <forddagujar@gmail.com>
2021-05-18 17:05:56 -0400

    sprints finalized

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App'
 
 import "react-datetime/css/react-datetime.css";
-import "react-multi-date-picker/styles/backgrounds/bg-gray.css"
+import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
 
 ReactDOM.render(
     <React.StrictMode>
