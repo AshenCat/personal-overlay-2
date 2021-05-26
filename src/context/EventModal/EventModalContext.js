7e4eb23b15244134347a0d96cc7e7640d1cea5e3
ashencat <forddagujar@gmail.com>
2021-05-25 21:55:26 -0400

    event modal major makeover (unchecked)

--- src/context/EventModal/EventModalContext.js
+++ src/context/EventModal/EventModalContext.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Button from '../../components/button/Button';
 import './eventmodalcontext.scss'
-import DateTime from 'react-datetime'
+// import DateTime from 'react-datetime'
 import moment from 'moment';
 import Modal from '../../components/modal/Modal';
 import Input from '../../components/input/inputText/Input';
 import SimpleCheckbox from '../../components/checkbox/simple/simpleCheckbox';
 import Textarea from '../../components/input/textarea/Textarea';
+import { Calendar } from 'react-multi-date-picker';
 
 const EventModalContext = React.createContext();
 
@@ -22,25 +23,40 @@ function EventModalProvider(props) {
     const [open, setEventModalOpen] = React.useState(false);
 
     const [title, setTitle] = React.useState("");
-    const [start, setStart] = React.useState("");
-    const [end, setEnd] = React.useState("");
+    // const [start, setStart] = React.useState("");
+    // const [end, setEnd] = React.useState("");
+    const [dates, setDates] = React.useState([])
     const [allDay, setAllDay] = React.useState(false)
     const [desc, setDesc] = React.useState("")
-    const [calendarRef, setCalendarRef] = React.useState({})
+    const [calendarRef, setCalendarRef] = React.useState(null)
+    // const [obj, setObj] = React.useState({})
+    // const [func, setFunc] = React.useState(null)
 
     const onSubmit = () => {
-        if (title === "" || start === "") return;
-        if (end !== "") if(moment(start).isAfter(end)) return;
+        if (title === "") return;
+        if (dates[1] && dates[1] !== "") if(moment(dates[0]?.toString()).isAfter(moment(dates[1]?.toString()))) return;
         // if (start === end) return;
         // console.log(sVal.format('MM-DD-YYYY HH:mm:ss'));
         // console.log(eVal.format('MM-DD-YYYY HH:mm:ss'));
-        calendarRef.current.getApi().addEvent({
+        const obj = {
             title, 
-            start: moment(start).toDate(), 
-            end: moment(end).toDate(),
+            start: moment(dates[0]?.toString()).toDate() ?? null, 
+            end: moment(dates[1]?.toString()).toDate() ?? null,
             allDay: allDay,
             description: desc,
-        });
+        }
+        // func(obj)
+        
+        if (calendarRef?.current) {
+            console.log('from home')
+            calendarRef.current.getApi().addEvent(obj);
+        }
+        if (calendarRef?.new) {
+            console.log('from edit sprint')
+            console.log(dates)
+            calendarRef.new.setSprint(obj)
+        }
+
         onClose();
     }
 
@@ -48,25 +64,37 @@ function EventModalProvider(props) {
         if(open) {
             console.log('clear')
             setTitle("");
-            setStart("");
-            setEnd("");
+            // setStart("");
+            // setEnd("");
             setAllDay(false);
             setDesc('');
         }
+        // setFunc(null)
+        setCalendarRef(null);
         setEventModalOpen(false);
     }
 
     return (
-        <EventModalContext.Provider value={{setEventModalOpen, setCalendarRef, setStart, setEnd}}>
+        <EventModalContext.Provider value={{setEventModalOpen, setCalendarRef, setDates}}>
             {props.children}
             <Modal open={open} setEventModalOpen={setEventModalOpen} onClose={onClose}>
                 <div className="title"><h3>Add Event</h3></div>
                 <div className="content">
-                    <div className="input-group">
-                        <label>Title: </label>
-                        <Input placeholder="Title" value={title} onChange={e=>setTitle(e.target.value)} mAndP={true}/>
+                    <div className="left">
+                        <div className="input-group">
+                            <label>Title: </label>
+                            <Input placeholder="Title" value={title} onChange={e=>setTitle(e.target.value)} mAndP={true}/>
+                        </div>
+                        <div className="input-group">
+                            <label>Description: </label>
+                            <Textarea value={desc} onChange={(e)=>setDesc(e.target.value)} className="textarea-modal"/>
+                        </div>
+                        <div className="input-group">
+                            <label>All day: </label>
+                            <SimpleCheckbox value={allDay} onClick={()=>setAllDay(prev=>!prev)} />
+                        </div>
                     </div>
-                    <div className="input-group">
+                    {/* <div className="input-group">
                         <label>Start Date: </label>
                         <DateTime 
                             value={start} 
@@ -84,20 +112,27 @@ function EventModalProvider(props) {
                             inputProps={{
                                 className: "datetime-input"
                             }}/>
-                    </div>
-                    <div className="input-group">
-                        <label>All day: </label>
-                        <SimpleCheckbox value={allDay} onClick={()=>setAllDay(prev=>!prev)} />
-                    </div>
-                    <div className="input-group">
-                        <label>Description: </label>
-                        <Textarea value={desc} onChange={(e)=>setDesc(e.target.value)} />
+                    </div> */}
+                    <div className="right">
+                        <div className="input-group" style={{flexFlow:"column"}}>
+                            <label>Range: </label>
+                            <Calendar
+                                value={dates}
+                                onChange={date=>{
+                                    setDates(date)
+                                    console.log(date)
+                                }}
+                                range
+                                showOtherDays
+                                className="modal-calendar bg-dark" />
+                        </div>
                     </div>
                 </div>
                 <div className="actions">
-                    <Button onClick={()=>setEventModalOpen(false)}>Cancel</Button>
-                    <Button style={{backgroundColor:"#1177BB", color:"white"}} onClick={onSubmit}>Submit</Button>
+                    <Button onClick={()=>setEventModalOpen(false)} className="cancel-btn">Cancel</Button>
+                    <Button style={{backgroundColor:"#1177BB", color:"white"}} className="submit" onClick={onSubmit}>Submit</Button>
                 </div>
+                
             </Modal>
         </EventModalContext.Provider>
     )
