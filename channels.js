64b78c87faa652e5e6f41c96c47b7d1d25fe9551
ashencat <forddagujar@gmail.com>
2021-05-27 20:43:48 -0400

    channel comms

--- channels.js
+++ channels.js
@@ -4,7 +4,9 @@ const rendererToMain = [
     "onSprintAdd",
     "LoadSprints",
     "LoadCalendarEvents",
-    "LoadSprint"
+    "LoadSprint",
+    "EditSprint",
+    "DeleteSprint"
 ]
 
 const mainToRenderer = [
@@ -12,7 +14,9 @@ const mainToRenderer = [
     "onSprintAdd",
     "LoadSprints",
     "LoadCalendarEvents",
-    "LoadSprint"
+    "LoadSprint",
+    "EditSprint",
+    "DeleteSprint"
 ]
 
 module.exports = {
