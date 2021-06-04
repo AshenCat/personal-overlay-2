e91f1557d355188f2751b2265cea700aa9e4396e
ashencat <forddagujar@gmail.com>
2021-06-03 22:04:51 -0400

    simple checkbox bugfix(?)

--- src/components/checkbox/simple/SimpleCheckbox.js
+++ src/components/checkbox/simple/SimpleCheckbox.js
@@ -4,7 +4,7 @@ import './simplecheckbox.scss'
 
 function SimpleCheckbox(props) {
     return (
-        <input type="checkbox" value={props.value} checked={props.value} onClick={props.onClick}/>
+        <input type="checkbox" value={props.value} checked={props.value} onChange={props.onClick}/>
     )
 }
 
