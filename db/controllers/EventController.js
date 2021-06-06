eb00ed7355aa52ae9239bb70db89044def162ec3
ashencat <forddagujar@gmail.com>
2021-06-06 15:23:15 -0400

    monkass fix

--- db/controllers/EventController.js
+++ db/controllers/EventController.js
@@ -69,34 +69,67 @@ const LoadSprint = async (e, id) => {
     }
 }
 
-const EditSprint = async (e, sprint) => {
+const EditSprint =  (e, sprint) => {
     const events = sprint.events;
     const del = sprint.del;
     delete sprint.events;
     delete sprint.del;    
+    
+    const newEvents = events.filter(e=>!e._id);
 
-    await SprintModel.findByIdAndUpdate(mongoose.Types.ObjectId(sprint._id), sprint, {new:true}, (err,doc)=>{
-        if(doc) console.log('findbyid: true')
+    if(newEvents.length > 0) {
+        EventModel.insertMany(events.filter(e=>!e._id), (err, docs)=>{
+            console.log(docs)
+            SprintModel.findByIdAndUpdate(mongoose.Types.ObjectId(sprint._id), sprint, {new:true}, (err,doc)=>{
+                if(doc) console.log('findbyid: true')
+        
+                if(del) {
+                    del.map( async (dl)=> {
+                        await doc.events.pull(dl._id)
+                        EventModel.findByIdAndDelete(mongoose.Types.ObjectId(sprint._id),{},(err, doc) => {
+                            console.log(`ID: ${dl._id} deleted!`)
+                            e.sender.send('DeletedEvent', `Successfully deleted event: ${dl._id}`)
+                        })
+                    })
+                }
 
-        if(del) {
-            del.map( async (dl)=> {
-                await doc.events.pull(dl._id)
-                EventModel.findByIdAndDelete(mongoose.Types.ObjectId(sprint._id),{},(err, doc) => {
-                    console.log(`ID: ${dl._id} deleted!`)
-                    e.sender.send('DeletedEvent', `Successfully deleted event: ${dl._id}`)
-                })
+                doc.events.push(docs.map(e=>e._id))
+        
+                // events.filter(event => !event._id).map( async (event) => {
+                //     const item = new EventModel(event);
+                //     const saved = await item.save();
+                //     if(saved) doc.events.push(item._id)
+                // });
+        
+                doc.save()
+                e.sender.send('EditSprint', `Successfully edited ${sprint._id}`); 
             })
-        }
-
-        events.filter(event => !event._id).map( async (event) => {
-            const item = new EventModel(event);
-            const saved = await item.save();
-            if(saved) doc.events.push(item._id)
-        });
-
-        doc.save()
-        e.sender.send('EditSprint', `Successfully edited ${sprint._id}`); 
-    })
+        })
+    }
+    else {
+        SprintModel.findByIdAndUpdate(mongoose.Types.ObjectId(sprint._id), sprint, {new:true}, (err,doc)=>{
+            if(doc) console.log('findbyid: true')
+    
+            if(del) {
+                del.map( async (dl)=> {
+                    await doc.events.pull(dl._id)
+                    EventModel.findByIdAndDelete(mongoose.Types.ObjectId(sprint._id),{},(err, doc) => {
+                        console.log(`ID: ${dl._id} deleted!`)
+                        e.sender.send('DeletedEvent', `Successfully deleted event: ${dl._id}`)
+                    })
+                })
+            }
+    
+            // events.filter(event => !event._id).map( async (event) => {
+            //     const item = new EventModel(event);
+            //     const saved = await item.save();
+            //     if(saved) doc.events.push(item._id)
+            // });
+    
+            doc.save()
+            e.sender.send('EditSprint', `Successfully edited ${sprint._id}`); 
+        })
+    }
 }
 
 const DeleteSprint = async (e, id) => {
