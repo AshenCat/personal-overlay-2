7bdc6c8ff15e4659862ba100f30d4a090e136513
ashencat <forddagujar@gmail.com>
2021-06-15 13:18:57 -0400

    tray and global accelerator

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const {
     app,
     BrowserWindow,
     ipcMain,
+    globalShortcut,
+    Tray,
+    Menu
     // Notification
 } = require('electron');
 const mongoose = require('mongoose').set('debug', true);
@@ -9,6 +12,14 @@ const path = require('path')
 
 const isDev = !app.isPackaged;
 let dbFailed = false;
+
+const version = 'v0.0.0'
+
+const trayMenu = Menu.buildFromTemplate([
+    {label: version},
+    {label: 'Check for updates'},
+    {role: 'quit'},
+])
 
 mongoose.connect(
     require('./db/config/config').db, {
@@ -23,9 +34,16 @@ mongoose.connect(
         dbFailed=true;
     });
 
-let win;
+let win, tray;
+
+function createTray() {
+    tray = new Tray('./icons/icon.png')
+    tray.setToolTip('Personal Overlay')
+    tray.setContextMenu(trayMenu)
+}
 
 function createWindow() {
+    createTray()
     win = new BrowserWindow({
         width:1366, minWidth:900,
         height: 768, minHeight: 650,
@@ -44,6 +62,14 @@ function createWindow() {
         if(dbFailed) {
             console.log('dbfailed')
             win.webContents.send('dbfailed', "Could not connect to mongoDB...");
+        }
+    })
+    globalShortcut.register('CommandOrControl+Shift+~', () => {
+        if (win.isVisible()) {
+            win.hide()
+            console.log('hide')
+        } else {
+            win.show()
         }
     })
 }
